e9eda53fdd8a079df7563ea6511aec2b034bcd6d
Adhitya Aditama Nugraha <aaditamanugraha@Adhityas-MacBook-Air.local>
2024-05-12 20:48:03 +0700

    fix: tailwind css style did'nt applied in html file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      yellow: "hsl(47, 88%, 63%)",
+      white: "hsl(0, 0%, 100%)",
+      grey: "hsl(0, 0%, 50%)",
+      black: "hsl(0, 0%, 7%)",
+    },
   },
   plugins: [],
 };
