c579a2d9a31dcc269e10f859a5106e5e0410e159
Adhitya Aditama Nugraha <aaditamanugraha@Adhityas-MacBook-Air.local>
2024-05-15 19:41:17 +0700

    style: styling main card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        custom: [
+          "box-shadow: 10px 10px 0px 0px rgba(0,0,0,0.75)",
+          "-webkit-box-shadow: 10px 10px 0px 0px rgba(0,0,0,0.75)",
+          "-moz-box-shadow: 10px 10px 0px 0px rgba(0,0,0,0.75)",
+        ],
+      },
+    },
     colors: {
       yellow: "hsl(47, 88%, 63%)",
       white: "hsl(0, 0%, 100%)",
